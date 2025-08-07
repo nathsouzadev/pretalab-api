964e42ea90d0423b8f3e5a316dc55d8c329177ea
kim-a9 <kimberlysantosalves@gmail.com>
2025-08-07 15:07:45 -0300

    test: teste unitário

--- tests/unit/app.spec.ts
+++ tests/unit/app.spec.ts
@@ -4,7 +4,11 @@ import app from "../../src/index";
 describe('Transactions API unit', () =>{
 
     it('deve iniciar servidor com sucesso', async () => {
-        expect(true).toEqual(true);
-        
+        const res = await app.get("/");
+
+        expect(res.status).toBe(200);
+        expect(res.body).toBe("Transactions API v1");
+        // expect(true).toEqual(true);
     });
+    
 });
