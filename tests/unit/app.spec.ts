b8b23fb18b4fbb7b865f65909153b6ed1cb940cf
kim-a9 <kimberlysantosalves@gmail.com>
2025-08-07 22:18:15 -0300

    testes unitarios

--- tests/unit/app.spec.ts
+++ tests/unit/app.spec.ts
@@ -1,14 +1,37 @@
 import { request } from "express";
+import transactionService from "../../src/service/transactionService";
 import app from "../../src/index";
 
 describe('Transactions API unit', () =>{
 
-    it('deve iniciar servidor com sucesso', async () => {
-        const res = await app.get("/");
+    it('deve criar uma transação com sucesso', async () => {
+         const newTransact = transactionService.createTransactions({
+            id: "12",
+            date: "2024-07-15T10:00:00Z",
+            description: "Despesas com remédios",
+            amount: 40.5,
+            type: "expense",
+            category: "Saúde",
+         });
 
-        expect(res.status).toBe(200);
-        expect(res.body).toBe("Transactions API v1");
-        // expect(true).toEqual(true);
+         expect(newTransact).toMatchObject({ transaction: newTransact });
+
     });
+
+    it('deve retornar erro caso um dos campos não esteja preenchido', async () => {
+        const newTransact = transactionService.createTransactions({
+            id: "12",
+            date: "2024-07-15T10:00:00Z",
+            description: "",
+            amount: 0,
+            type: "expense",
+            category: "",
+         });
+
+         if(!newTransact.description || !newTransact.amount || !newTransact.category) {
+            expect(newTransact).rejects.toThrow('Todos os campos devem estar preenchidos');
+         };
+    });
+
     
 });
