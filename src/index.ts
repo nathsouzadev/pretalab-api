d4149a0b8cff2a6073e3527029240fc2e1da8854
Angela Carvalho <angelalopo@outlook.com>
2025-08-11 18:17:03 -0300

    feat: restructure API server by consolidating app and server logic into app.ts and removing server.ts

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,7 @@
-import express from "express";
-import { transactions } from "./data";
-import { getTransactionsById } from "./services/transactionsService";
-import { createTransactionController } from "./controller/createTransaction";
+import app from "./app";
 
-const app = express();
-
-app.use(express.json());
-
-app.get("/", (_req, res) => {
-  res.json({ message: "Transactions API v2" });
+const server = app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
 
-
-app.get("/transactions", (_req, res) => {
-  res.json({ transactions });
-});
-
-
-app.get("/transactions/:id", (req, res) => {
-  const { id } = req.params;
-  const transaction = getTransactionsById(id);
-  if (!transaction) {
-    return res.status(404).json({ message: "Transação não encontrada" });
-  }
-  res.json({ transaction });
-});
-
-
-app.post("/transactions", (req, res) => {
-  createTransactionController(req, res);
-});
-
-export default app;
+export default server;
--- src/server.ts
+++
@@ -1,7 +0,0 @@
-import app from "./index";
-
-const server = app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
-
-export default server;
