c798efad33a3ce8c06921f29124d67f3c342bd08
kim-a9 <kimberlysantosalves@gmail.com>
2025-08-14 20:45:43 -0300

    test: testes unit e integration

--- tests/integration/app.spec.ts
+++ tests/integration/app.spec.ts
@@ -1,9 +1,10 @@
 import request from "supertest";
 import app from "../../src/index";
 
-describe("Transactions API", () => {
+describe("Transactions API - Integrations", () => {
   it("should return a 200 status code", async () => {
     const response = await request(app).get("/");
-    expect(response.status).toBe(200);
+    expect(response.statusCode).toBe(200);
+    expect(response.body.message).toEqual("Transactions API v1");
   });
 });
--- tests/unit/app.spec.ts
+++ tests/unit/app.spec.ts
@@ -1,37 +1,16 @@
-import { request } from "express";
-import transactionService from "../../src/service/transactionService";
-import app from "../../src/index";
+// import { createTransactions } from '../../service/transactionService';
+import {transactions} from '../../src/data';
 
-describe('Transactions API unit', () =>{
 
-    it('deve criar uma transação com sucesso', async () => {
-         const newTransact = transactionService.createTransactions({
-            id: "12",
-            date: "2024-07-15T10:00:00Z",
-            description: "Despesas com remédios",
-            amount: 40.5,
-            type: "expense",
-            category: "Saúde",
-         });
+describe('Transactions API - Testes unitários', () =>{
 
-         expect(newTransact).toMatchObject({ transaction: newTransact });
+it('deve retornar uma array de transações', () => {
 
-    });
+   expect(transactions.length).toBeGreaterThan(0);
 
-    it('deve retornar erro caso um dos campos não esteja preenchido', async () => {
-        const newTransact = transactionService.createTransactions({
-            id: "12",
-            date: "2024-07-15T10:00:00Z",
-            description: "",
-            amount: 0,
-            type: "expense",
-            category: "",
-         });
+   });
 
-         if(!newTransact.description || !newTransact.amount || !newTransact.category) {
-            expect(newTransact).rejects.toThrow('Todos os campos devem estar preenchidos');
-         };
-    });
 
-    
+   
 });
+  
