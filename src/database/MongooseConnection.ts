e79fff3422dc262471da1cc75b326e472a3abde8
kim-a9 <kimberlysantosalves@gmail.com>
2025-08-16 20:13:55 -0300

    fix: conexão ao mongodb

--- src/database/MongooseConnection.ts
+++ src/database/MongooseConnection.ts
@@ -4,6 +4,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export async function mongoConnect() {
+    const MONGO_URL = process.env.MONGO_URL;
+
+    if(!MONGO_URL){
+        throw new Error('Não foi possível conectar ao MongoDB');
+    }
+    
     try {
         await mongoose.connect(process.env.MONGO_URL!);
         console.log('Conexão ao MongoDB realizada com sucesso!');
--- tests/integration/app.spec.ts
+++ tests/integration/app.spec.ts
@@ -1,18 +1,9 @@
 import request from "supertest";
-import mongoose from 'mongoose';
 import app from "../../src/index";
-// import {TransactionsModel} from "../../src/database/MongooseTransactionModel";
 
 describe("Transactions API - Integrations", () => {
 
-  // beforeAll(async () => {
-  //   await mongoose.createConnection(process.env.MONGO_URL_TESTE!);
-  // });
-
-  // afterAll(async () => {
-  //   await TransactionsModel.deleteMany({});
-  //   await mongoose.connection.close();
-  // }); 
+ 
   it("should return a 200 status code", async () => {
     const response = await request(app).get("/");
     expect(response.statusCode).toBe(200);
