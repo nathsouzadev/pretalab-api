3c8b6fe69689fa1d5a4301da6dd882e4ad40105f
juliana <user.taniajuju91@gmail.com>
2025-08-28 15:07:44 -0300

    Rota Get

--- src/controllers/ai.ts
+++ src/controllers/ai.ts
@@ -1,5 +1,6 @@
 import {Response, Request} from "express";
 import { ai, aiChatInteration } from "../services/prompt"
+import { HistoricoModel } from "../database/mongooseGeminiModel";
 
  export const aiResponse = async (req: Request, res: Response) => {
   const { prompt } = req.body;
@@ -18,3 +19,14 @@ import { ai, aiChatInteration } from "../services/prompt"
   res.json(response)
 
 };
+
+export const getAiChatResponse = async (req: Request, res: Response) => {
+  try {
+    const history = await HistoricoModel.findOne({});
+    if (!history) {
+      return res.json({ interactions: [] });
+    }
+    res.json(history.interactions);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao carregar o histórico do chat.' });
+}};
--- src/routes/ai-routes.ts
+++ src/routes/ai-routes.ts
@@ -1,10 +1,12 @@
 import express from 'express';
-import { aiResponse, aiChatResponse } from "../controllers/ai";
+import { aiResponse, aiChatResponse, getAiChatResponse } from "../controllers/ai";
 
 var router = express.Router();
 
 router.post("/ai", async (req, res) => aiResponse(req,res))
 
 router.post("/chat", async (req, res) => aiChatResponse(req,res))
+
+router.get("/chat", getAiChatResponse )
 
 export default router;
